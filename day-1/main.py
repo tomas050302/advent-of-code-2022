b1a558cfe8c93f6025d72ef35752efa283b3b7aa
Tomás Lopes <tomas050302@gmail.com>
2022-12-01 12:38:27 +0000

    added part 1 & 2 answers

--- day-1/main.py
+++ day-1/main.py
@@ -1,23 +1,20 @@
 def main():
     with open('input.txt', 'r') as f:
-        lines = f.readlines()
         current_sum = 0
         sums = []
-        i = 0
-        # foreach line
-        for line in lines:
-            if (line == "\n"):
+        for line in f.readlines():
+            line = line.strip()
+            if line == '':
                 sums.append(current_sum)
-                i += 1
                 current_sum = 0
                 continue
-            line = int(line)
-            current_sum += line
+            current_sum += int(line)
 
         sums.sort()
         sums.reverse()
         print()
-        print(sums[0]+sums[1]+sums[2])
+        print("Part 1 answer: " + str(sums[0]))
+        print("Part 2 answer: " + str(sums[0]+sums[1]+sums[2]))
 
 
 main()
